21aff9455e1d26eeceb79302b03db6e4c35c426a
lenovo <lenovo@DESKTOP-5T9GHPM>
2025-09-03 14:26:03 +0530

    j

--- CoreJava/src/Day1/TypeConvertionDemo.java
+++ CoreJava/src/Day1/TypeConvertionDemo.java
@@ -6,14 +6,21 @@ package Day1;
 public class TypeConvertionDemo {
 
 	public static void main(String[] args) {
-		byte num1 = 127;
+		/*byte num1 = 127;
 		int num2 = num1;
 		
 		int num3 = 257;   // num3%256(range of byte)
 		byte num4 = (byte)num3;
 		
 		System.out.println(num4);
+*/
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
 
+		
+		System.out.println(a+b);
+		
 	}
 
 }
