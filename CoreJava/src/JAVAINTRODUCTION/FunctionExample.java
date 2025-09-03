2219575c8b6d792ccca6cf2ca2c3afd2a2c59994
lenovo <lenovo@192.168.1.37>
2025-09-03 22:17:11 +0530

    INTROOOOOOOOOO

---
+++ CoreJava/src/JAVAINTRODUCTION/FunctionExample.java
@@ -0,0 +1,12 @@
+package JAVAINTRODUCTION;
+
+public class FunctionExample {
+
+    public static void greet() {
+        System.out.println("Welcome to Java Programming!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
